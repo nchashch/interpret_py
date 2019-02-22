1f7149743e580d9f1edfb1e53e53c9a613f6f0b8
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-02-22 11:24:37 +0300

    Fix regexp support in Lexer
    * Now it is greedy

--- lexer.py
+++ lexer.py
@@ -4,21 +4,27 @@ class Lexer:
     def __init__(self, terminals):
         assert type(terminals) == dict
         self._terminals = terminals
-        self._acc = []
 
     def next_token(self, text):
-        self._acc = []
-        for i, c in enumerate(text):
-            self._acc.append(c)
+        acc = []
+        for i, c0 in enumerate(text):
+            acc.append(c0)
             for t in self._terminals:
                 terminal = self._terminals[t]
-                acc = ''.join(self._acc)
-                if re.match(terminal, acc):
-                    return (t, acc)
+                acc_str = ''.join(acc)
+                if re.match(terminal, acc_str):
+                    for c1 in text[i+1:]:
+                        acc_str = ''.join(acc)
+                        if re.match(terminal, acc_str + c1):
+                            acc.append(c1)
+                            acc_str = ''.join(acc)
+                        else:
+                            return t, acc_str
+                    if re.match(terminal, acc_str):
+                        return t, acc_str
         return '', ''
 
     def tokenize(self, text):
-        tokens = []
         while text:
             t, acc = self.next_token(text)
             token = (t, acc)
@@ -26,16 +32,17 @@ class Lexer:
             if len(acc) == 0:
                 text = text[1:]
                 continue
-            tokens.append(token)
-        return tokens
+            yield token
 
 if __name__ == '__main__':
     terminals = {
-        'abcd': 'abcd',
-        'ABCD': 'ABCD',
-        'DIGIT': '[0-9]',
+        'abcd': '^abcd$',
+        'ABCD': '^ABCD$',
+        'NUM': '^0|([1-9][0-9]{0,})$',
+        'DIGIT': '^[0-9]$',
     }
     lex = Lexer(terminals)
     text = '1234 abcd abcd, ABCD, abcd, 1 23 2442'
-    print(lex.next_token(text))
-    print(lex.tokenize(text))
+    print(text + '\n')
+    for token in lex.tokenize(text):
+        print(token)
