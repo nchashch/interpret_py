c007286b5f35265acd1c637a808270c62dd5f65c
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-03-16 16:02:00 +0300

    Reword comment in Lexer class

--- lexer.py
+++ lexer.py
@@ -19,8 +19,9 @@ class Lexer:
             c1 = text[i+1]
             terms_c0.clear()
             terms_c1.clear()
-            # reversed in order to make terminals
-            # further up have higher priority
+            # reversed so that terminals
+            # further up in the OrderedDict
+            # have higher priority
             for t in reversed(self._terminals):
                 terminal = self._terminals[t]
                 match0 = re.match(terminal, acc_str)
@@ -70,22 +71,24 @@ if __name__ == '__main__':
         ('L_CB', '^{$'),
         ('R_CB', '^}$'),
 
+        ('L_B', '^\($'),
+        ('R_B', '^\)$'),
+
+        ('SEMICOLON', '^;$'),
+
         ('PRINT_KW', '^print$'),
         ('WHILE_KW', '^while$'),
         ('IF_KW', '^if$'),
         ('NUM', '^0$|^([1-9][0-9]{0,})$'),
         ('IDENT', '^[a-zA-Z]+[a-zA-Z0-9]*$'),
     ])
-    # var reg = /^(o|$)(n|$)(e|$)(\s|$)$/;
-
     lex = Lexer(terminals)
-
-    # if 1 == 1 || 1 >= 10 {
-    #     A = 1 + 2;
-    #     print A;
-    # }
-
     text = '''
+    if 1 == 1 || 1 >= 10 {
+        A = (1 + 2);
+        print A;
+    }
+
     while cat <= 10 && cat >= 0 {
         A = 1;
         cat = cat + A;
